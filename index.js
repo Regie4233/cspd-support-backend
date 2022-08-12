fe7311c81e7093e5b990a05147131bc9e35e3293
Regie4233 <regie4233@ymail.com>
2022-08-12 16:59:28 -0400

    initial build deploy 0.25

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(cors({
     origin: ['https://superb-churros-56b022.netlify.app', 'https://superb-churros-56b022.netlify.app/reporter', 'https://superb-churros-56b022.netlify.app/viewer', 'https://superb-churros-56b022.netlify.app/login'],
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true,
+    allowedHeaders: origin
 }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
