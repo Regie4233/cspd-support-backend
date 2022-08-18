dfbe96bd31a84ba32373d96dd021a01293dc2ac5
Regie4233 <regie4233@ymail.com>
2022-08-18 18:13:27 -0400

    refractoring room GET logic'less hardcoding'

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-let or1 = [];
-let or2 = [];
+
+// let room_arr = [{
+//     or1: [], or2: [], or3: [], or4: [], or5: [], or6: [], or6: [], or7: [], or8: [], or9: [], or10: [],
+//     or11: [], or12: [], or13: [], or14: [], or15: [], or16: [], or17: [], or18: [], or19: [], or20: [],
+//     or21: [], or22: [], or23: [], or24: [], or25: [], or26: [], or26: [], or27: [], or28: [], or29: [],  or30: [],
+//     or31: [], or32: []
+// }];
+let room_arr = [];
 
 const db = mysql.createConnection({
     host: 'us-cdbr-east-06.cleardb.net',
@@ -31,7 +37,7 @@ const db = mysql.createConnection({
 
 // });
 app.use(cors({
-    origin: ['https://regie4233.github.io','https://regie4233.github.io/cspd-support', 'https://regie4233.github.io/cspd-support/reporter', 'https://regie4233.github.io/cspd-support/viewer', 'https://regie4233.github.io/cspd-support/login'],
+    origin: ['https://regie4233.github.io', 'https://regie4233.github.io/cspd-support', 'https://regie4233.github.io/cspd-support/reporter', 'https://regie4233.github.io/cspd-support/viewer', 'https://regie4233.github.io/cspd-support/login'],
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true,
 }));
@@ -108,124 +114,145 @@ app.post('/api/insert', (req, res) => {
 app.get('/api/get/traydata', (req, res) => {
     db.query('SELECT * FROM trayinfo', (err, result) => {
         if (err) console.log(err); //took out throw eerro
-        let temp_or1 = []; 
-        let temp_or12 = [];let temp_or13 = [];
-        let temp_or2 = []; let temp_or14 = [];
-        let temp_or3 = []; let temp_or15 = [];
-        let temp_or4 = []; let temp_or16 = [];
-        let temp_or5 = []; let temp_or17 = [];
-        let temp_or6 = []; let temp_or18 = [];
-        let temp_or7 = []; let temp_or19 = [];
-        let temp_or8 = []; let temp_or20 = [];
-        let temp_or9 = []; let temp_or21 = [];
-        let temp_or10 = []; let temp_or22 = [];
-        let temp_or24 = [];let temp_or27 = [];
-        let temp_or11 = []; let temp_or25 = [];
-        let temp_or23 = []; let temp_or28 = [];
-        let temp_or26 = []; let temp_or31 = [];
-        let temp_or29 = []; let temp_or32 = [];
-        let temp_or30 = [];
-        for (let i = 0; i < result.length; i++) {
-            if (result[i].roomnum === 1) {
-                temp_or1.push(result[i]);
-            } else if (result[i].roomnum === 2) {
-                temp_or2.push(result[i]);
-            } else if (result[i].roomnum === 3) {
-                temp_or3.push(result[i]);
-            } else if (result[i].roomnum === 4) {
-                temp_or4.push(result[i]);
-            } else if (result[i].roomnum === 5) {
-                temp_or5.push(result[i]);
-            } else if (result[i].roomnum === 6) {
-                temp_or6.push(result[i]);
-            } else if (result[i].roomnum === 7) {
-                temp_or7.push(result[i]);
-            } else if (result[i].roomnum === 8) {
-                temp_or8.push(result[i]);
-            } else if (result[i].roomnum === 9) {
-                temp_or9.push(result[i]);
-            } else if (result[i].roomnum === 10) {
-                temp_or10.push(result[i]);
-            }else if (result[i].roomnum === 11) {
-                temp_or11.push(result[i]);
-            } else if (result[i].roomnum === 12) {
-                temp_or12.push(result[i]);
-            } else if (result[i].roomnum === 13) {
-                temp_or13.push(result[i]);
-            } else if (result[i].roomnum === 14) {
-                temp_or14.push(result[i]);
-            } else if (result[i].roomnum === 15) {
-                temp_or15.push(result[i]);
-            } else if (result[i].roomnum === 16) {
-                temp_or16.push(result[i]);
-            } else if (result[i].roomnum === 17) {
-                temp_or17.push(result[i]);
-            } else if (result[i].roomnum === 18) {
-                temp_or18.push(result[i]);
-            } else if (result[i].roomnum === 19) {
-                temp_or19.push(result[i]);
-            } else if (result[i].roomnum === 20) {
-                temp_or20.push(result[i]);
-            } else if (result[i].roomnum === 21) {
-                temp_or21.push(result[i]);
-            } else if (result[i].roomnum === 22) {
-                temp_or22.push(result[i]);
-            } else if (result[i].roomnum === 23) {
-                temp_or23.push(result[i]);
-            } else if (result[i].roomnum === 24) {
-                temp_or24.push(result[i]);
-            } else if (result[i].roomnum === 25) {
-                temp_or25.push(result[i]);
-            } else if (result[i].roomnum === 26) {
-                temp_or26.push(result[i]);
-            } else if (result[i].roomnum === 27) {
-                temp_or27.push(result[i]);
-            } else if (result[i].roomnum === 28) {
-                temp_or28.push(result[i]);
-            } else if (result[i].roomnum === 29) {
-                temp_or29.push(result[i]);
-            } else if (result[i].roomnum === 30) {
-                temp_or30.push(result[i]);
-            }else if (result[i].roomnum === 31) {
-                temp_or31.push(result[i]);
-            } else if (result[i].roomnum === 32) {
-                temp_or32.push(result[i]);
+
+        // let temp_or1 = []; 
+        // let temp_or12 = [];let temp_or13 = [];
+        // let temp_or2 = []; let temp_or14 = [];
+        // let temp_or3 = []; let temp_or15 = [];
+        // let temp_or4 = []; let temp_or16 = [];
+        // let temp_or5 = []; let temp_or17 = [];
+        // let temp_or6 = []; let temp_or18 = [];
+        // let temp_or7 = []; let temp_or19 = [];
+        // let temp_or8 = []; let temp_or20 = [];
+        // let temp_or9 = []; let temp_or21 = [];
+        // let temp_or10 = []; let temp_or22 = [];
+        // let temp_or24 = [];let temp_or27 = [];
+        // let temp_or11 = []; let temp_or25 = [];
+        // let temp_or23 = []; let temp_or28 = [];
+        // let temp_or26 = []; let temp_or31 = [];
+        // let temp_or29 = []; let temp_or32 = [];
+        // let temp_or30 = [];
+        // for (let i = 0; i < result.length; i++) {
+        //     if (result[i].roomnum === 1) {
+        //         temp_or1.push(result[i]);
+        //     } else if (result[i].roomnum === 2) {
+        //         temp_or2.push(result[i]);
+        //     } else if (result[i].roomnum === 3) {
+        //         temp_or3.push(result[i]);
+        //     } else if (result[i].roomnum === 4) {
+        //         temp_or4.push(result[i]);
+        //     } else if (result[i].roomnum === 5) {
+        //         temp_or5.push(result[i]);
+        //     } else if (result[i].roomnum === 6) {
+        //         temp_or6.push(result[i]);
+        //     } else if (result[i].roomnum === 7) {
+        //         temp_or7.push(result[i]);
+        //     } else if (result[i].roomnum === 8) {
+        //         temp_or8.push(result[i]);
+        //     } else if (result[i].roomnum === 9) {
+        //         temp_or9.push(result[i]);
+        //     } else if (result[i].roomnum === 10) {
+        //         temp_or10.push(result[i]);
+        //     }else if (result[i].roomnum === 11) {
+        //         temp_or11.push(result[i]);
+        //     } else if (result[i].roomnum === 12) {
+        //         temp_or12.push(result[i]);
+        //     } else if (result[i].roomnum === 13) {
+        //         temp_or13.push(result[i]);
+        //     } else if (result[i].roomnum === 14) {
+        //         temp_or14.push(result[i]);
+        //     } else if (result[i].roomnum === 15) {
+        //         temp_or15.push(result[i]);
+        //     } else if (result[i].roomnum === 16) {
+        //         temp_or16.push(result[i]);
+        //     } else if (result[i].roomnum === 17) {
+        //         temp_or17.push(result[i]);
+        //     } else if (result[i].roomnum === 18) {
+        //         temp_or18.push(result[i]);
+        //     } else if (result[i].roomnum === 19) {
+        //         temp_or19.push(result[i]);
+        //     } else if (result[i].roomnum === 20) {
+        //         temp_or20.push(result[i]);
+        //     } else if (result[i].roomnum === 21) {
+        //         temp_or21.push(result[i]);
+        //     } else if (result[i].roomnum === 22) {
+        //         temp_or22.push(result[i]);
+        //     } else if (result[i].roomnum === 23) {
+        //         temp_or23.push(result[i]);
+        //     } else if (result[i].roomnum === 24) {
+        //         temp_or24.push(result[i]);
+        //     } else if (result[i].roomnum === 25) {
+        //         temp_or25.push(result[i]);
+        //     } else if (result[i].roomnum === 26) {
+        //         temp_or26.push(result[i]);
+        //     } else if (result[i].roomnum === 27) {
+        //         temp_or27.push(result[i]);
+        //     } else if (result[i].roomnum === 28) {
+        //         temp_or28.push(result[i]);
+        //     } else if (result[i].roomnum === 29) {
+        //         temp_or29.push(result[i]);
+        //     } else if (result[i].roomnum === 30) {
+        //         temp_or30.push(result[i]);
+        //     }else if (result[i].roomnum === 31) {
+        //         temp_or31.push(result[i]);
+        //     } else if (result[i].roomnum === 32) {
+        //         temp_or32.push(result[i]);
+        //     }
+        //     or1 = temp_or1;
+        //     or2 = temp_or2;
+        //     or3 = temp_or3;
+        //     or4 = temp_or4;
+        //     or5 = temp_or5;
+        //     or6 = temp_or6;
+        //     or7 = temp_or7;
+        //     or8 = temp_or8;
+        //     or9 = temp_or9;
+        //     or10 = temp_or10;
+        //     or11 = temp_or11;
+        //     or12 = temp_or12;
+        //     or13 = temp_or13;
+        //     or14 = temp_or14;
+        //     or15 = temp_or15;
+        //     or16 = temp_or16;
+        //     or17 = temp_or17;
+        //     or18 = temp_or18;
+        //     or19 = temp_or19;
+        //     or20 = temp_or20;
+        //     or21 = temp_or21;
+        //     or22 = temp_or22;
+        //     or23 = temp_or23;
+        //     or24 = temp_or24;
+        //     or25 = temp_or25;
+        //     or26 = temp_or26;
+        //     or27 = temp_or27;
+        //     or28 = temp_or28;
+        //     or29 = temp_or29;
+        //     or30 = temp_or30;
+        //     or31 = temp_or31;
+        //     or32 = temp_or32;
+        // }
+        // res.send({ or1, or2, or3, or4, or5, or6, or7, or8, or9, or10, or11, or12, or13, or14, or15, or16, or17, or18, or19, or20,
+        //     or21, or22, or23, or24, or25, or26, or27, or28, or29, or30, or31, or32});
+        for (let i = 0; i < 32; i++) {
+
+            let temp_arr = [];
+
+            for (let y = 0; y < result.length; y++) {
+
+                if (result[i].roomnum == i) {
+                    temp_arr.push(result[i]);
+                }
             }
-            or1 = temp_or1;
-            or2 = temp_or2;
-            or3 = temp_or3;
-            or4 = temp_or4;
-            or5 = temp_or5;
-            or6 = temp_or6;
-            or7 = temp_or7;
-            or8 = temp_or8;
-            or9 = temp_or9;
-            or10 = temp_or10;
-            or11 = temp_or11;
-            or12 = temp_or12;
-            or13 = temp_or13;
-            or14 = temp_or14;
-            or15 = temp_or15;
-            or16 = temp_or16;
-            or17 = temp_or17;
-            or18 = temp_or18;
-            or19 = temp_or19;
-            or20 = temp_or20;
-            or21 = temp_or21;
-            or22 = temp_or22;
-            or23 = temp_or23;
-            or24 = temp_or24;
-            or25 = temp_or25;
-            or26 = temp_or26;
-            or27 = temp_or27;
-            or28 = temp_or28;
-            or29 = temp_or29;
-            or30 = temp_or30;
-            or31 = temp_or31;
-            or32 = temp_or32;
+
+            room_arr[i] = temp_arr;
+
         }
-        res.send({ or1, or2, or3, or4, or5, or6, or7, or8, or9, or10, or11, or12, or13, or14, or15, or16, or17, or18, or19, or20,
-            or21, or22, or23, or24, or25, or26, or27, or28, or29, or30, or31, or32});
+        res.send({
+            or1: room_arr[0], or2: room_arr[1], or3: room_arr[2], or4: room_arr[3], or5: room_arr[4], or6: room_arr[5], or7: room_arr[6], or8: room_arr[7], or9: room_arr[8], or10: room_arr[9], 
+            or11: room_arr[10], or12: room_arr[11], or13: room_arr[12], or14: room_arr[13], or15: room_arr[14], or16: room_arr[15], or17: room_arr[16], or18: room_arr[17], or19: room_arr[18], or20: room_arr[19],
+            or21: room_arr[20], or22: room_arr[21], or23: room_arr[22], or24: room_arr[23], or25: room_arr[24], or26: room_arr[25], or27: room_arr[26], or28: room_arr[27], or29: room_arr[28], or30: room_arr[29], 
+            or31: room_arr[30], or32: room_arr[31]
+        });
 
     });
 });
