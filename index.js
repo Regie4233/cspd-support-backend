31903133128352fe5f08167f5da4600ec91739e8
Regie4233 <regie4233@ymail.com>
2022-08-18 19:17:01 -0400

    refractoring room GET logic

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ let or29 = [];
 let or30 = [];
 let or31 = [];
 let or32 = [];
-const room_arr = [or1, or2, or3, or4, or5, or6, or7, or8, or9, or10, or11, or12, or13, or14, or15, or16, or17, or18, or19, or20,
+let room_arr = [or1, or2, or3, or4, or5, or6, or7, or8, or9, or10, or11, or12, or13, or14, or15, or16, or17, or18, or19, or20,
     or21, or22, or23, or24, or25, or26, or27, or28, or29, or30, or31, or32];
 // let room_arr = [];
 
@@ -261,7 +261,7 @@ app.get('/api/get/traydata', (req, res) => {
         // res.send({ or1, or2, or3, or4, or5, or6, or7, or8, or9, or10, or11, or12, or13, or14, or15, or16, or17, or18, or19, or20,
         //     or21, or22, or23, or24, or25, or26, or27, or28, or29, or30, or31, or32});
         for (let i = 0; i < 31; i++) {
-            const temp_arr = [];
+            let temp_arr = [];
             for (let y = 0; y < result.length; y++) {
                 if (result[y].roomnum === i) {
                     temp_arr.push(result[y]);
