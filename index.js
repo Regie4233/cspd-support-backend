2c0fd2136e2cc4c376e73f21bb779de824d8ca30
Regie4233 <regie4233@ymail.com>
2022-08-18 20:10:54 -0400

    refractoring room GET logic

--- index.js
+++ index.js
@@ -8,38 +8,38 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// let or1 = [];
-// let or2 = [];
-// let or3 = [];
-// let or4 = [];
-// let or5 = [];
-// let or6 = [];
-// let or7 = [];
-// let or8 = [];
-// let or9 = [];
-// let or10 = [];
-// let or11 = [];
-// let or12 = [];
-// let or13 = [];
-// let or14 = [];
-// let or15 = [];
-// let or16 = [];
-// let or17 = [];
-// let or18 = [];
-// let or19 = [];
-// let or20 = [];
-// let or21 = [];
-// let or22 = [];
-// let or23 = [];
-// let or24 = [];
-// let or25 = [];
-// let or26 = [];
-// let or27 = [];
-// let or28 = [];
-// let or29 = [];
-// let or30 = [];
-// let or31 = [];
-// let or32 = [];
+let or1 = [];
+let or2 = [];
+let or3 = [];
+let or4 = [];
+let or5 = [];
+let or6 = [];
+let or7 = [];
+let or8 = [];
+let or9 = [];
+let or10 = [];
+let or11 = [];
+let or12 = [];
+let or13 = [];
+let or14 = [];
+let or15 = [];
+let or16 = [];
+let or17 = [];
+let or18 = [];
+let or19 = [];
+let or20 = [];
+let or21 = [];
+let or22 = [];
+let or23 = [];
+let or24 = [];
+let or25 = [];
+let or26 = [];
+let or27 = [];
+let or28 = [];
+let or29 = [];
+let or30 = [];
+let or31 = [];
+let or32 = [];
 // let room_arr = [or1, or2, or3, or4, or5, or6, or7, or8, or9, or10, or11, or12, or13, or14, or15, or16, or17, or18, or19, or20,
 //     or21, or22, or23, or24, or25, or26, or27, or28, or29, or30, or31, or32];
 
@@ -138,54 +138,119 @@ app.post('/api/insert', (req, res) => {
 
 // }
 app.get('/api/get/traydata', (req, res) => {
-    let or1 = [];
-    let or2 = [];
-    let or3 = [];
-    let or4 = [];
-    let or5 = [];
-    let or6 = [];
-    let or7 = [];
-    let or8 = [];
-    let or9 = [];
-    let or10 = [];
-    let or11 = [];
-    let or12 = [];
-    let or13 = [];
-    let or14 = [];
-    let or15 = [];
-    let or16 = [];
-    let or17 = [];
-    let or18 = [];
-    let or19 = [];
-    let or20 = [];
-    let or21 = [];
-    let or22 = [];
-    let or23 = [];
-    let or24 = [];
-    let or25 = [];
-    let or26 = [];
-    let or27 = [];
-    let or28 = [];
-    let or29 = [];
-    let or30 = [];
-    let or31 = [];
-    let or32 = [];
-    let room_arr = [or1, or2, or3, or4, or5, or6, or7, or8, or9, or10, or11, or12, or13, or14, or15, or16, or17, or18, or19, or20,
-            or21, or22, or23, or24, or25, or26, or27, or28, or29, or30, or31, or32];
+    
     db.query('SELECT * FROM trayinfo', (err, result) => {
         if (err) console.log(err); //took out throw eerro
 
 
-        
-        for (let i = 0; i < 32; i++) {
+        let temp_or1 = []; let temp_or2 = []; let temp_or3 = []; let temp_or4 = []; let temp_or5 = [];
+        let temp_or6 = []; let temp_or7 = []; let temp_or8 = []; let temp_or9 = []; let temp_or10 = [];
+        let temp_or11 = []; let temp_or12 = []; let temp_or13 = []; let temp_or14 = []; let temp_or15 = [];
+        let temp_or16 = []; let temp_or17 = []; let temp_or18 = []; let temp_or19 = []; let temp_or20 = [];
+        let temp_or21 = []; let temp_or22 = []; let temp_or23 = []; let temp_or24 = []; let temp_or25 = [];
+        let temp_or26 = []; let temp_or27 = []; let temp_or28 = []; let temp_or29 = []; let temp_or30 = [];
+        let temp_or31 = []; let temp_or32 = [];
 
-            for (let y = 0; y < result.length; y++) {
-                if (result[y].roomnum === i) {
-                    room_arr[i].push(result[y]);
-                }
+        for(let i = 0; i < result.length; i++){
+            if(result[i].roomnum === 1){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 2){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 3){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 4){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 5){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 6){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 7){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 8){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 9){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 10){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 11){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 12){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 13){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 14){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 15){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 16){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 17){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 18){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 19){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 20){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 21){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 22){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 23){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 24){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 25){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 26){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 27){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 28){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 29){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 30){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 31){
+                temp_or1.push(result[i]);
+            } else if(result[i].roomnum === 32){
+                temp_or1.push(result[i]);
             }
-
+            or1 = temp_or1;
+            or2 = temp_or2;
+            or3 = temp_or3;
+            or4 = temp_or4;
+            or5 = temp_or5;
+            or6 = temp_or6;
+            or7 = temp_or7;
+            or8 = temp_or8;
+            or9 = temp_or9;
+            or10 = temp_or10;
+            or11 = temp_or11;
+            or12 = temp_or12;
+            or13 = temp_or13;
+            or14 = temp_or14;
+            or15 = temp_or15;
+            or16 = temp_or16;
+            or17 = temp_or17;
+            or18 = temp_or18;
+            or19 = temp_or19;
+            or20 = temp_or20;
+            or21 = temp_or21;
+            or22 = temp_or22;
+            or23 = temp_or23;
+            or24 = temp_or24;
+            or25 = temp_or25;
+            or26 = temp_or26;
+            or27 = temp_or27;
+            or28 = temp_or28;
+            or29 = temp_or29;
+            or30 = temp_or30;
+            or31 = temp_or31;
+            or32 = temp_or32;
         }
+
         res.send({
             or1, or2, or3, or4, or5, or6, or7, or8, or9, or10, or11, or12, or13, or14, or15, or16, or17, or18, or19, or20,
             or21, or22, or23, or24, or25, or26, or27, or28, or29, or30, or31, or32
