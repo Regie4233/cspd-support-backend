816b01d6779c0c29d1cae4693d7d896eb58cadd3
Regie4233 <regie4233@ymail.com>
2022-08-31 20:12:51 -0400

    adding truncate all feature

--- index.js
+++ index.js
@@ -326,6 +326,7 @@ app.put('/api/update/trayname', (req, res) => { //work in progress
 });
 
 app.delete('/api/truncate', (req, res) => {
+    console.log('start truncate');
     for(let i = 1; i < 4; i++){
         db.query(`TRUNCATE TABLE case_number_${i}`, (err, result)=>{
             if(err) { console.log('truncate error... ' + err); }
